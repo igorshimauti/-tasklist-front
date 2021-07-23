29395f060df04d31b4468266ab23a1fd6313542e
igor.shimauti <igor.shimauti@linx.com.br>
2021-07-23 11:22:47 -0300

    Ajuste para CORS

--- src/app/components/task-crud/task.service.ts
+++ src/app/components/task-crud/task.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { EMPTY, Observable } from 'rxjs';
@@ -11,33 +11,45 @@ import { Task } from './task.model';
 export class TaskService {
 
   baseUrl = "https://ec2-3-142-255-209.us-east-2.compute.amazonaws.com:8443/tasklist/task";
+  httpHeader = new HttpHeaders({
+    "Access-Control-Allow-Origin": "https://tasklist-supero.netlify.app",
+    "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Max-Age": "86400"    
+  });
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
   create(task: Task): Observable<Task> {
-    return this.http.post<Task>(this.baseUrl, task).pipe(
+    return this.http.post<Task>(this.baseUrl, task, { headers: this.httpHeader }).pipe(
       map(obj => obj),
       catchError(e => this.errorHandler(e))
     );
   }
 
   read(): Observable<Task[]> {
-    return this.http.get<Task[]>(this.baseUrl);
+    return this.http.get<Task[]>(this.baseUrl, { headers: this.httpHeader });
   }
 
   readById(id: string): Observable<Task> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.get<Task>(url);
+    return this.http.get<Task>(url, { headers: this.httpHeader });
   }
 
   update(task: Task): Observable<Task> {
     const url = `${this.baseUrl}/${task.id}`;
-    return this.http.put<Task>(url, task);
+    return this.http.put<Task>(url, task, { headers: this.httpHeader }).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
   }
 
   delete(task: Task): Observable<Task> {
     const url = `${this.baseUrl}/${task.id}`;
-    return this.http.delete<Task>(url);
+    return this.http.delete<Task>(url, { headers: this.httpHeader }).pipe(
+      map(obj => obj),
+      catchError(e => this.errorHandler(e))
+    );
   }
 
   showMessage(msg: string, isError: boolean = false): void {
